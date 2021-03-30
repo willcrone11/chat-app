f98ecda9126942ef953601595d2e265fade6f878
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-03-30 12:40:02 -0400

    Adds firestore to the project for storing messages

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
   
@@ -8,10 +11,52 @@ export default class Chat extends React.Component {
     super();
     this.state = {
       messages: [],
+      user: '',
+      uid: '',
     }
+
+    //initializes firestore
+    var firebaseConfig = {
+      apiKey: "AIzaSyDibp0m7CyuLs2Za1OLgg_Z96-A-Pr_Vh0",
+      authDomain: "chat-app-31787.firebaseapp.com",
+      projectId: "chat-app-31787",
+      storageBucket: "chat-app-31787.appspot.com",
+      messagingSenderId: "1021412781283",
+      appId: "1:1021412781283:web:449dadcb53d5df278a29d6",
+      measurementId: "G-8DD7KZ01NG"
+    };
+    
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    
+    this.referenceChatMessages = firebase.firestore().collection("messages");
   }
 
   componentDidMount() {
+
+    this.referenceChatMessages = firebase.firestore().collection('messages');
+    this.unsubscribe = this.referenceChatMessages.onSnapshot(this.onCollectionUpdate);
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      //updates state with current user data
+      this.setState({
+        uid: user.uid,
+        messages: [],
+      });
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
+
+    this.setState({
+      user: this.props.route.params.name,
+    })
+
+    /* 
     let { name } = this.props.route.params;
     this.setState({
       messages: [
@@ -32,14 +77,53 @@ export default class Chat extends React.Component {
           system: true,
         },
       ],
-    })
+    }) */
   }
 
-  //the message a user has just sent gets appended to the state 'messages' so that it can be displayed in the chat.
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
+ }
+
+ //Updates the messages in the state every time they change on the firestore
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+        //get the QueryDocumentSnapshot's data
+        let data = doc.data();
+        messages.push({
+            _id: data._id,
+            text: data.text,
+            createdAt: data.createdAt.toDate(),
+            user: data.user
+        });
+    });
+    this.setState({
+        messages,
+    });
+  }
+
+  // Adds messages to the current chat log through GiftedChat; state remains unaffected
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    }),
+    () => {
+      this.addMessage();
+    }
+  );
+    
+  }
+
+  //Adds messages to the firebase.
+  addMessage() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
   }
 
   //changes text bubble color for users (targeted using 'right'; 'left' would be used to target received messages)
