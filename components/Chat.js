5014e5f1573a39cdb080f1655e1183cb6109d31b
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-04-12 19:11:08 -0400

    Adds functionality for adding photos and location to chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { View, Alert, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
+import { View, Alert, Platform, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-community/async-storage';
 import NetInfo from '@react-native-community/netinfo';
+import CustomActions from './CustomActions';
+import MapView from 'react-native-maps';
+import { renderInputToolbar, renderComposer, renderSend } from './InputToolbar';
 
 const firebase = require('firebase');
 require('firebase/firestore');
@@ -20,6 +23,8 @@ export default class Chat extends React.Component {
       },
       uid: '',
       isConnected: false,
+      image: null,
+      location: null,
     }
 
     //initializes firestore
@@ -70,12 +75,7 @@ export default class Chat extends React.Component {
         .orderBy("createdAt", "desc")
         .onSnapshot(this.onCollectionUpdate);
       });
-
-      this.setState({
-        user: this.props.route.params.name,
-      })
-
-      } else {
+    } else {
         console.log("offline");
         Alert.alert('No internet connection, unable to send messages');
         this.setState({
@@ -125,7 +125,13 @@ export default class Chat extends React.Component {
             _id: data._id,
             text: data.text,
             createdAt: data.createdAt.toDate(),
-            user: data.user
+            user: {
+              _id: data.user._id,
+              name: data.user.name,
+              avatar: data.user.avatar
+            },
+            image: data.image || '',
+            location: data.location || null
         });
     });
     this.setState({ messages });
@@ -148,9 +154,15 @@ export default class Chat extends React.Component {
     const message = this.state.messages[0];
     this.referenceChatMessages.add({
       _id: message._id,
-      text: message.text,
+      text: message.text || '',
       createdAt: message.createdAt,
-      user: message.user,
+      user: {
+        _id: message.user._id,
+        name: message.user.name,
+        avatar: message.user.avatar,
+      },
+      image: message.image || '',
+      location: message.location || null
     });
   }
 
@@ -191,27 +203,42 @@ export default class Chat extends React.Component {
   //changes text bubble color for users (targeted using 'right'; 'left' would be used to target received messages)
   renderBubble(props) {
     return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: '#000'
-          }
-        }}
-      />
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            right: {
+              backgroundColor: '#147EFB'
+            },
+            left: {
+              backgroundColor: '#F0F1EF'
+            }
+          }}
+        />
     )
   }
 
-  //if user is offline input for writing new messages will not appear
-  renderInputToolbar(props) {
-    if (this.state.isConnected == false) {
-    } else {
-      return(
-        <InputToolbar
-        {...props}
+  //renders action button for adding photos, location etc.
+  renderCustomActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+
+  // Returns a MapView that shows user's location
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
         />
       );
     }
+    return null;
   }
   
   render() {
@@ -219,23 +246,36 @@ export default class Chat extends React.Component {
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
     //defines background color prop user selected on start screen
-    let color  = this.props.route.params.color;
+    let color = this.props.route.params.color;
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff'
+      }
+    });
 
     return (
-      <View
-        style={{flex: 1, backgroundColor: color }}
-      >
+      <View style={{flex: 1, backgroundColor: color }}>
         <GiftedChat
           renderBubble={this.renderBubble.bind(this)}
-          renderInputToolbar={this.renderInputToolbar.bind(this)}
+          renderInputToolbar={renderInputToolbar}
+          renderComposer={renderComposer}
+          renderSend={renderSend}
+          renderActions={this.renderCustomActions}
+          renderCustomView={this.renderCustomView}
+          showUserAvatar
+          isTyping
+          //renderUsernameOnMessage
           style={{backgroundColor: color }}
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
-          placeholder={this.state.isConnected ? 'Type a message...' : 'No internet connection'}
+          placeholder={'Type a message...'}
+          maxInputLength={this.state.isConnected ? 2000 : 0}
           user={{
             _id: this.state.user._id,
             avatar: this.state.user.avatar,
-            name: { name },
+            name: { name } ,
           }}
         />
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
